0b112463e2f510d12bc1709aaf141a640febbdd5
ipcarepi <hackedipod@naver.com>
2025-10-28 13:13:43 +0900

    project setting

---
+++ greenmap/src/main/java/com/ucamp/greenmap/GreenmapApplication.java
@@ -0,0 +1,13 @@
+package com.ucamp.greenmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenmapApplication.class, args);
+	}
+
+}
---
+++ greenmap/src/test/java/com/ucamp/greenmap/GreenmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucamp.greenmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
