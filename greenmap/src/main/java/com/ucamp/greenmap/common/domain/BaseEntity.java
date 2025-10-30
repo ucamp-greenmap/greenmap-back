80906e52705c5a2928dd572f06fe7f10884504ee
ryuyena0305 <ryuyena0305@gmail.com>
2025-10-30 13:02:18 +0900

    Refactor: BaseEntity 수정

--- greenmap/src/main/java/com/ucamp/greenmap/common/domain/BaseEntity.java
+++ greenmap/src/main/java/com/ucamp/greenmap/common/domain/BaseEntity.java
@@ -1,17 +1,22 @@
 package com.ucamp.greenmap.common.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @CreatedDate
     @Column(name = "created_at")
     protected LocalDateTime createdAt;
 
@@ -19,5 +24,5 @@ public class BaseEntity {
     protected LocalDateTime updatedAt;
 
     @Column(name = "is_active")
-    protected Boolean isActive;
+    protected Boolean isActive = true;
 }
