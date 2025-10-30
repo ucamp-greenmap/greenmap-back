f88eea1d271c44a17c31d767ec43a4fc1f74f978
ipcarepi <hackedipod@naver.com>
2025-10-30 17:04:26 +0900

    Fix: 모든 컨트롤러에 CrossOrigin 추가

--- greenmap/src/main/java/com/ucamp/greenmap/Kakao/controller/CustomErrorController.java
+++ greenmap/src/main/java/com/ucamp/greenmap/Kakao/controller/CustomErrorController.java
@@ -2,9 +2,11 @@ package com.ucamp.greenmap.Kakao.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:5173")
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
