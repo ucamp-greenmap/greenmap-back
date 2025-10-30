4c3404f0402dda6996b4457b620c0368de1ca64b
ipcarepi <hackedipod@naver.com>
2025-10-30 13:53:59 +0900

    Refactor: 뉴스 관련 DTO 어노테이션 수정

--- greenmap/src/main/java/com/ucamp/greenmap/news/dto/request/NewsRequest.java
+++ greenmap/src/main/java/com/ucamp/greenmap/news/dto/request/NewsRequest.java
@@ -1,12 +1,8 @@
 package com.ucamp.greenmap.news.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class NewsRequest {
     private Long memberId;
     private String title;
