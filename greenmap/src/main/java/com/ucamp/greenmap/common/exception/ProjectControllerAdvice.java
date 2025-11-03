e76b1997186d7e5eb25e91132223f7b5a4c87f0c
ipcarepi <hackedipod@naver.com>
2025-11-03 18:44:27 +0900

    Fix: stacktrace 추가

--- greenmap/src/main/java/com/ucamp/greenmap/common/exception/ProjectControllerAdvice.java
+++ greenmap/src/main/java/com/ucamp/greenmap/common/exception/ProjectControllerAdvice.java
@@ -5,10 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ProjectControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
-        return ResponseEntity.ok(ApiResponse.error("서버 오류가 발생했습니다: " + e.getStackTrace()));
+        String stackTrace = Arrays.stream(e.getStackTrace())
+                .map(StackTraceElement::toString)
+                .collect(Collectors.joining("\n"));
+        return ResponseEntity.ok(ApiResponse.error("서버 오류:\n" + stackTrace));
     }
 }
