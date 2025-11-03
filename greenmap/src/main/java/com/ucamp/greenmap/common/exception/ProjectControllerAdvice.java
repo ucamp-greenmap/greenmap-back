a2ff93c7e0e3272865322915bd42debe345803ec
paul <cebu13@gmail.com>
2025-11-03 18:22:13 +0900

    Fix: Cloud Run 배포 타임아웃을 600초로 증가

--- greenmap/src/main/java/com/ucamp/greenmap/common/exception/ProjectControllerAdvice.java
+++ greenmap/src/main/java/com/ucamp/greenmap/common/exception/ProjectControllerAdvice.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ProjectControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
-        return ResponseEntity.ok(ApiResponse.error("서버 오류가 발생했습니다: " + e.getStackTrace()));
+        return ResponseEntity.ok(ApiResponse.error("서버 오류가 발생했습니다: " + e.getStackTrace().toString()));
     }
 }
