e47555115cb6806e9d825b06b8b89c1bd9b81555
sai06266 <sai06266@naver.com>
2025-11-04 20:18:27 +0900

    Refactor: 카테고리 목록 수정

--- greenmap/src/main/java/com/ucamp/greenmap/common/domain/CategoryName.java
+++ greenmap/src/main/java/com/ucamp/greenmap/common/domain/CategoryName.java
@@ -3,11 +3,13 @@ package com.ucamp.greenmap.common.domain;
 public enum CategoryName {
     BIKE,
     ZERO_WASTE,
-    CAR,
+    EVCAR,
+    HCAR,
     RECYCLING_CENTER,
     NEWS,
     CHALLENGE,
     CASH,
     VOUCHER,
-    BOOKMARK
+    BOOKMARK,
+    BADGE
 }
