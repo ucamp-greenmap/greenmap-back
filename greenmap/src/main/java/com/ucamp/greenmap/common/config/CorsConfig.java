77fcc2bad82bc1f55570b1815779a16bc5d2c74c
ipcarepi <hackedipod@naver.com>
2025-11-04 18:56:18 +0900

    Fix: Cors 설정 fix

--- greenmap/src/main/java/com/ucamp/greenmap/common/config/CorsConfig.java
+++ greenmap/src/main/java/com/ucamp/greenmap/common/config/CorsConfig.java
@@ -1,11 +1,11 @@
 package com.ucamp.greenmap.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebFluxConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
