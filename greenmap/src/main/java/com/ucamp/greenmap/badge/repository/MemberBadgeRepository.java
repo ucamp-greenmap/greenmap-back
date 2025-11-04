e438223d0158ab9781a2235929f8b35c5e372f46
ipcarepi <hackedipod@naver.com>
2025-11-04 13:41:03 +0900

    Refactor: 인증 관련 로직 로그인 memberId 사용, 미사용 import 정리 + orElseThrow 메시지 처리

--- greenmap/src/main/java/com/ucamp/greenmap/badge/repository/MemberBadgeRepository.java
+++ greenmap/src/main/java/com/ucamp/greenmap/badge/repository/MemberBadgeRepository.java
@@ -3,7 +3,6 @@ package com.ucamp.greenmap.badge.repository;
 import com.ucamp.greenmap.badge.domain.MemberBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
